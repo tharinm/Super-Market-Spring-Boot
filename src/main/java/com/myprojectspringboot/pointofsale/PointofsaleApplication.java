68e798dfe8b29d4d451ee273c078a38726d6bb08
tharinm <tharindumadhushan864@gmail.com>
2024-08-27 21:41:14 +0530

    initial commit

---
+++ src/main/java/com/myprojectspringboot/pointofsale/PointofsaleApplication.java
@@ -0,0 +1,13 @@
+package com.myprojectspringboot.pointofsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointofsaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointofsaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myprojectspringboot/pointofsale/PointofsaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojectspringboot.pointofsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointofsaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
