8c311f068414ff536c5c871752319b6c9472c17d
tharinm <tharindumadhushan864@gmail.com>
2024-08-28 01:05:09 +0530

    check routes get response from postman

--- src/main/java/com/myprojectspringboot/pointofsale/controller/TestController.java
+++ src/main/java/com/myprojectspringboot/pointofsale/controller/TestController.java
@@ -1,4 +1,27 @@
 package com.myprojectspringboot.pointofsale.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/v1/test")
 public class TestController {
+
+
+    @GetMapping("/get-text-1")
+    public String getMyTest(){
+        String myTest="This is my first spring boot app";
+        System.out.println(" "+myTest);
+        return myTest;
+    }
+
+    @GetMapping("/get-text-2")
+    public String  getMyTest2(){
+        String myTest="This is my first spring boot app print 2";
+        System.out.println(" "+myTest);
+        return myTest;
+    }
 }
