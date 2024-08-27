f5da33d0b0c127f4c9949ac3ff944877b6d5bf83
tharinm <tharindumadhushan864@gmail.com>
2024-08-28 01:11:19 +0530

    make customer class

---
+++ src/main/java/com/myprojectspringboot/pointofsale/controller/Customer.java
@@ -0,0 +1,28 @@
+package com.myprojectspringboot.pointofsale.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/v1/customer")
+public class Customer {
+
+
+    @GetMapping("/get-text-1")
+    public String getMyTest(){
+        String myTest="This is cus 1";
+        System.out.println(" "+myTest);
+        return myTest;
+    }
+
+    @GetMapping("/get-text-2")
+    public String  getMyTest2(){
+        String myTest="This is cus 2";
+        System.out.println(" "+myTest);
+        return myTest;
+    }
+}
