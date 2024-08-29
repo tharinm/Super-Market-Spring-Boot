83094267ca7a2e20f424a60d0acd8ed742b808f6
tharinm <tharindumadhushan864@gmail.com>
2024-08-29 23:18:24 +0530

    working on entity

---
+++ src/main/java/com/myprojectspringboot/pointofsale/entity/Customer.java
@@ -0,0 +1,34 @@
+package com.myprojectspringboot.pointofsale.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.Array;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.ArrayList;
+
+@Entity
+@Table(name="customer")
+
+public class Customer {
+
+    @Id //primary key
+    @Column (name="customer_id",length = 45)  //column name
+    private int customerId;
+
+
+    @Column( name="customer_name",length = 100,nullable = false)
+    private String customerName;
+
+
+    @Column(name="customer_address",length = 255)
+    private String customerAddress;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(name="contact_numbers",length = 255,columnDefinition = "json")
+    private ArrayList contactNumber;
+}
