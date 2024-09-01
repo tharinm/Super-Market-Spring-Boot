40da9297be6928a92a6f9e716bbf0480538eed64
tharinm <tharindumadhushan864@gmail.com>
2024-09-02 00:19:58 +0530

    data successfuly insert to table

--- src/main/java/com/myprojectspringboot/pointofsale/repo/CustomerRepo.java
+++ src/main/java/com/myprojectspringboot/pointofsale/repo/CustomerRepo.java
@@ -1,4 +1,11 @@
 package com.myprojectspringboot.pointofsale.repo;
 
-public interface CustomerRepo  {
+import com.myprojectspringboot.pointofsale.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@EnableJpaRepositories
+public interface CustomerRepo extends JpaRepository <Customer,Integer > {
 }
--- src/main/java/com/myprojectspringboot/pointofsale/service/impl/customerServiceImpl.java
+++ src/main/java/com/myprojectspringboot/pointofsale/service/impl/customerServiceImpl.java
@@ -1,14 +1,37 @@
 package com.myprojectspringboot.pointofsale.service.impl;
 
 import com.myprojectspringboot.pointofsale.dto.CustomerDTO;
+import com.myprojectspringboot.pointofsale.entity.Customer;
+import com.myprojectspringboot.pointofsale.repo.CustomerRepo;
 import com.myprojectspringboot.pointofsale.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service
+
+
+
 public class customerServiceImpl implements CustomerService {
+
+    @Autowired
+    private CustomerRepo customerRepo;
+
     @Override
     public String saveCustomer(CustomerDTO customerDTO) {
+        //this is  constructor only no object
+        Customer customer=new Customer(
+                customerDTO.getCustomerId(),
+                customerDTO.getCustomerName(),
+                customerDTO.getCustomerAddress(),
+                customerDTO.getContactNumber(),
+                customerDTO.getCustomerSalary(),
+                customerDTO.getNic(),
+                customerDTO.isActive()
+        );
+
+
+        customerRepo.save(customer);
         System.out.println("address " +customerDTO.getCustomerAddress());
         return "";
     }
