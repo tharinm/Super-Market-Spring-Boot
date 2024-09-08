6c08e3b10b640580c0531d26c13875be5cf9e43e
tharinm <th4rinonline@gmail.com>
2024-09-08 11:01:59 +0530

    enum types enum annotation

---
+++ src/main/java/com/myprojectspringboot/pointofsale/entity/Item.java
@@ -0,0 +1,38 @@
+package com.myprojectspringboot.pointofsale.entity;
+
+
+import com.myprojectspringboot.pointofsale.entity.enums.MeasuringUnitType;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "item")
+public class Item {
+
+    @Id //primary key
+    @Column(name="item_id",length = 45)  //column name
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int customerId;
+
+    @Column( name="item_name",length = 100,nullable = false)
+    private String itemName;
+
+
+    //ENUM Class
+    @Enumerated(EnumType.STRING)
+    @Column( name="measure_type",length = 100,nullable = false)
+    private MeasuringUnitType measuringType;
+
+    @Column( name="balance_qty",length = 100,nullable = false)
+    private double QTY;
+
+    @Column( name="supplier_price",length = 100,nullable = false)
+    private double supplierPrice;
+
+    @Column( name="selling_price",length = 100,nullable = false)
+    private double sellingPrice;
+
+    @Column(name="active_status",columnDefinition = "TINYINT default 1")
+    private boolean activeState;
+
+
+}
---
+++ src/main/java/com/myprojectspringboot/pointofsale/entity/enums/MeasuringUnitType.java
@@ -0,0 +1,5 @@
+package com.myprojectspringboot.pointofsale.entity.enums;
+
+public enum MeasuringUnitType {
+    KILO_GRAM,GRAM,MILI_LITER,LITER,NUMBER
+}
