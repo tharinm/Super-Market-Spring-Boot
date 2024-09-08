15bf168bd17380f394a8f33a2e43a16d6392aee0
tharinm <th4rinonline@gmail.com>
2024-09-08 23:40:56 +0530

    save item succes

--- src/main/java/com/myprojectspringboot/pointofsale/service/impl/itemServiceImpl.java
+++ src/main/java/com/myprojectspringboot/pointofsale/service/impl/itemServiceImpl.java
@@ -6,8 +6,10 @@ import com.myprojectspringboot.pointofsale.entity.Item;
 import com.myprojectspringboot.pointofsale.entity.enums.MeasuringUnitType;
 import com.myprojectspringboot.pointofsale.repo.ItemRepo;
 import com.myprojectspringboot.pointofsale.service.itemService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.yaml.snakeyaml.constructor.DuplicateKeyException;
 
 
 @Service
@@ -16,22 +18,31 @@ public class itemServiceImpl implements itemService {
     @Autowired
     private ItemRepo itemRepo;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     @Override
     public String saveItem(ItemSaveRequestDTO itemSaveRequestDTO) {
-        Item item=new Item(
-               1,
-                itemSaveRequestDTO.getItemName(),
-                itemSaveRequestDTO.getMeasuringType(),
-                itemSaveRequestDTO.getQTY(),
-                itemSaveRequestDTO.getSupplierPrice(),
-                itemSaveRequestDTO.getSellingPrice(),
-                true
-        );
+//        Item item=new Item(
+//               1,
+//                itemSaveRequestDTO.getItemName(),
+//                itemSaveRequestDTO.getMeasuringType(),
+//                itemSaveRequestDTO.getQTY(),
+//                itemSaveRequestDTO.getSupplierPrice(),
+//                itemSaveRequestDTO.getSellingPrice(),
+//                true
+//        );
 
 
-        itemRepo.save(item);
+        Item item  =modelMapper.map(itemSaveRequestDTO,Item.class);
 
-        return item.getItemName();
+        if(!itemRepo.existsById(item.getItemId())){
+            itemRepo.save(item);
+            return item.getItemName() + "Saved Item successfully";
+        }
+        else {
+            throw new RuntimeException("Already Insert");
+        }
     }
 }
 
