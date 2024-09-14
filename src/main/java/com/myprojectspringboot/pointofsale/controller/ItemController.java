10e55ef8fdb450ef9c35cb5975cfb45fdc314cd3
tharinm <tharindumadhushan864@gmail.com>
2024-09-14 17:52:45 +0530

    response entity

--- src/main/java/com/myprojectspringboot/pointofsale/controller/ItemController.java
+++ src/main/java/com/myprojectspringboot/pointofsale/controller/ItemController.java
@@ -6,7 +6,10 @@ import com.myprojectspringboot.pointofsale.dto.request.ItemSaveRequestDTO;
 import com.myprojectspringboot.pointofsale.dto.response.ItemGetResponse;
 import com.myprojectspringboot.pointofsale.entity.Item;
 import com.myprojectspringboot.pointofsale.service.itemService;
+import com.myprojectspringboot.pointofsale.utills.StandardResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,11 +22,19 @@ public class ItemController {
     @Autowired
     private itemService itemService;
 
-    @PostMapping("/save")
-    public String saveItem(@RequestBody ItemSaveRequestDTO itemSaveRequestDTO) {
+//    @PostMapping("/save")
+//    public String saveItem(@RequestBody ItemSaveRequestDTO itemSaveRequestDTO) {
+//
+//      String message=  itemService.saveItem(itemSaveRequestDTO);
+//        return "saved";
+//    }
 
-      String message=  itemService.saveItem(itemSaveRequestDTO);
-        return "saved";
+    @PostMapping("/save")
+    public ResponseEntity<StandardResponse>saveItem(@RequestBody ItemSaveRequestDTO itemSaveRequestDTO) {
+
+        String message=  itemService.saveItem(itemSaveRequestDTO);
+         ResponseEntity<StandardResponse> response=new ResponseEntity<StandardResponse>(new StandardResponse(201,"success",message), HttpStatus.CREATED);
+        return  response;
     }
 
     @GetMapping(path="/get-item-by-name",params="name")
